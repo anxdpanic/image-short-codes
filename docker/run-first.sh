140abbf590921505caa662070e7631bdec13d111
anxdpanic <anxdpanic@@users.noreply.github.com>
2024-04-21 16:10:46 -0400

    ask for password

--- docker/run-first.sh
+++ docker/run-first.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-# TODO: ask for password
+echo -n "Password for sftp user: "
+read -rs PLAIN_TEXT_PASSWORD
 
-docker run --rm python:alpine python -c "import crypt; print(crypt.crypt('YOUR_PASSWORD'))"
-
+ENCRYPTED_PASSWORD=$(docker run --rm python:alpine python -W ignore::DeprecationWarning -c "import crypt; print(crypt.crypt('$PLAIN_TEXT_PASSWORD'))")
+ENCRYPTED_PASSWORD=${ENCRYPTED_PASSWORD//$/\$$}
+# awk -v password="$ENCRYPTED_PASSWORD" '{sub(/SFTP_PASSWORD=/,SFTP_PASSWORD=password); print}' .env
 # TODO: add encrypted password to .env
 
 ssh-keygen -t ed25519 -f ./ssh/ssh_host_ed25519_key
