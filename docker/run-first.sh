2679504a4106b7b402a20fec45c99d9a653b28af
anxdpanic <anxdpanic@@users.noreply.github.com>
2024-04-21 14:42:49 -0400

    add TODOs

--- docker/run-first.sh
+++ docker/run-first.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
+
+# TODO: ask for password
+
 docker run --rm python:alpine python -c "import crypt; print(crypt.crypt('YOUR_PASSWORD'))"
+
+# TODO: add encrypted password to .env
 
 ssh-keygen -t ed25519 -f ./ssh/ssh_host_ed25519_key
 ssh-keygen -t rsa -b 4096 -f ./ssh/ssh_host_rsa_key
