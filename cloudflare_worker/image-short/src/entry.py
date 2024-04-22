c8d04e3cd04941c0b2ca6c6206f2b188ec3e324a
anxdpanic <anxdpanic@@users.noreply.github.com>
2024-04-21 21:51:00 -0400

    add basic error/message page template

--- cloudflare_worker/image-short/src/entry.py
+++ cloudflare_worker/image-short/src/entry.py
@@ -1,14 +1,30 @@
 from db.schema import shortcodes_schema
+from static.message import message_html
 
 from js import Response
+
+
+def message_response(title, message):
+    message_body = message_html
+    for key, value in locals().items():
+        message_body = message_body.replace(f'%%{key}%%', value)
+
+    return message_body
 
 
 async def prepare_database(env):
     result = await env.image_db.prepare(shortcodes_schema).run()
     if not result.success:
-        return Response.new('Failed to prepare database')
+        payload = message_response('Error', 'Failed to create table in database')
+        response = Response.new(payload, {'status': 500})
+        response.headers.set('Content-Type', 'text/html')
+        return response
 
 
 async def on_fetch(request, env):
     await prepare_database(env)
+    payload = message_response('Success', 'Successful')
+    response = Response.new(payload, {'status': 200})
+    response.headers.set('Content-Type', 'text/html')
 
+    return response
