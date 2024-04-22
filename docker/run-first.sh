1eadfad1edf6258acff9ffe7d7e3c771008d33dd
anxdpanic <anxdpanic@@users.noreply.github.com>
2024-04-21 22:33:14 -0400

    use awk to add encrypted password to .env file

--- docker/run-first.sh
+++ docker/run-first.sh
@@ -4,11 +4,13 @@ echo -n "Password for sftp user: "
 read -rs PLAIN_TEXT_PASSWORD
 
 ENCRYPTED_PASSWORD=$(docker run --rm python:alpine python -W ignore::DeprecationWarning -c "import crypt; print(crypt.crypt('$PLAIN_TEXT_PASSWORD'))")
-ENCRYPTED_PASSWORD=${ENCRYPTED_PASSWORD//$/\$$}
-# awk -v password="$ENCRYPTED_PASSWORD" '{sub(/SFTP_PASSWORD=/,SFTP_PASSWORD=password); print}' .env
-# TODO: add encrypted password to .env
+ENV_ENTRY="SFTP_PASSWORD=${ENCRYPTED_PASSWORD//$/\$$}"
+
+awk -i inplace -v INPLACE_SUFFIX=.bak -v env_entry="$ENV_ENTRY" '{sub(/SFTP_PASSWORD=/,env_entry); print}' .env
 
 ssh-keygen -t ed25519 -f ./ssh/ssh_host_ed25519_key
 ssh-keygen -t rsa -b 4096 -f ./ssh/ssh_host_rsa_key
 
 mkdir data
+
+nano .env
