7160964b3ea1dbf62981d0a5a739f2237c54127e
anxdpanic <anxdpanic@@users.noreply.github.com>
2024-04-23 08:30:55 -0400

    rename .env so it is not overwritten by git

--- docker/run-first.sh
+++ docker/run-first.sh
@@ -6,6 +6,7 @@ read -rs PLAIN_TEXT_PASSWORD
 ENCRYPTED_PASSWORD=$(docker run --rm python:alpine python -W ignore::DeprecationWarning -c "import crypt; print(crypt.crypt('$PLAIN_TEXT_PASSWORD'))")
 ENV_ENTRY="SFTP_PASSWORD=${ENCRYPTED_PASSWORD//$/\$$}"
 
+cp .env.template .env
 awk -i inplace -v INPLACE_SUFFIX=.bak -v env_entry="$ENV_ENTRY" '{sub(/SFTP_PASSWORD=/,env_entry); print}' .env
 
 ssh-keygen -t ed25519 -f ./ssh/ssh_host_ed25519_key
