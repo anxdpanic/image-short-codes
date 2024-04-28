550319b9bacea504ae702f5731842d9ab53f1c57
anxdpanic <anxdpanic@@users.noreply.github.com>
2024-04-28 13:08:31 -0400

    fixup pip install

--- watchdog/setup.py
+++ watchdog/setup.py
@@ -1,7 +1,19 @@
+from pathlib import Path
+from pathlib import PurePath
+
 import setuptools
 
 from watchdog.__init__ import *
 
+__short_description__ = ('A watchdog that monitors a directory for changes to image files, mirrors changes to a '
+                         'remote sftp server, when necessary generates a shortcode url using a Cloudflare worker, '
+                         'then sends a Discord notification with the generated shortcode url.')
+
+with Path(PurePath(Path(__file__).cwd()).parent, 'README.md').open() as file_handle:
+    __long_description__ = file_handle.read()
+
+with Path('requirements.txt').open() as file_handle:
+    __requirements__ = file_handle.read().splitlines()
 
 setuptools.setup(
     name=__name__,
@@ -10,18 +22,18 @@ setuptools.setup(
     author_email=__email__,
     description=__short_description__,
     long_description=__long_description__,
-    long_description_content_type="text/markdown",
+    long_description_content_type='text/markdown',
     url=__github__,
     project_urls={
-        "Bug Tracker": f"{__github__}/issues",
+        'Bug Tracker': f'{__github__}/issues',
     },
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: OS Independent',
     ],
     packages=setuptools.find_packages(),
-    python_requires=">=3.6",
+    python_requires='>=3.6',
     install_requires=__requirements__,
     entry_points={'console_scripts': ['watchdog-imgshort=watchdog.__main__:main']},
 )
